493324f91ee6eb38ea00a9194ef4e46b019eb379
Jake Russell <russelljd2@jacks.sfasu.edu>
2026-03-29 17:04:21 -0500

    Take Assessment page set up and works with populated user assessments

---
+++ FinancialLiteracyTool/Pages/UserPages/TakeAssessment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinancialLiteracyTool.Pages.UserPages
+{
+    public class TakeAssessmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

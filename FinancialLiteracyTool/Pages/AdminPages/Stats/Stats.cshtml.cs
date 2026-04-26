1c884db2cfab113f9bec90c6b23d2e8b10da2bcf
Rmando42 <aremcaar@outlook.com>
2026-04-26 05:20:38 -0500

    Added a stats page and began working on it

---
+++ FinancialLiteracyTool/Pages/AdminPages/Stats/Stats.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinancialLiteracyTool.Pages.AdminPages
+{
+    public class StatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
